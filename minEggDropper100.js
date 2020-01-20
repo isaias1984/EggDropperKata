e662bd34bafaab28551df264e01c067fe8873e25
Isaias Redondo <isaiasredondovilchez@gmail.com>
2020-01-20 17:31:23 +0100

    rethinking the basic level with only one egg

--- minEggDropper100.js
+++ minEggDropper100.js
@@ -1,22 +1,20 @@
 /** Basic Difficulty */
 
-/* I asume everything starts at the 100-floor and ends when a egg is not broken.
-   I asume that because: If an egg survives a fall from floor n , then it 
-   would also survive a fall from the floors below it: floor n-1 , floor n-2 , etc. */
+/* I was rethinking the problem and I detected a problem, if I assume all start at 100 floor and 
+I only have one egg and I drop it, if it breaks I can't assume that the critical floor is where I am, 
+or is some of the floors below me. However, if I'm on the first floor and I drop an egg, if it breaks 
+I can assume that the critical floor is the zero floor. Is more logic for me and is a possibly start to 
+think in more eggs or infinite eggs. And the rules about what happens when an egg survives or not 
+(if it survives at n floor can survive at n-1, n-2 ... And if not can't survive at floors n+1, n+2) 
+are considered too.. */
 
-function minEggDropper100() {
-    let eggs = 100;
-    let minDrops100 = 0;
+function minEggDropper() {
+    let minDrops = 0;
 
-    while (eggs >= 1) {
-        minDrops100++;
-        if (eggBroken()) {
-            eggs--;
-        } else {
-            return minDrops100;
-        }
+    while (!eggBroken()) {
+        minDrops++;
     }
-    return minDrops100;
+    return minDrops;
 }
 
 /** Random function to simulate the consequence of egg drop */
@@ -26,7 +24,7 @@ function eggBroken() {
 }
 
 function prueba() {
-    console.log(minEggDropper100());
+    console.log(minEggDropper());
 }
 
 prueba();
