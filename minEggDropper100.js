022ee66eaf888f5a356a876c2a7eab18cd7e4ba4
Isaias Redondo <isaiasredondovilchez@gmail.com>
2020-01-20 18:17:15 +0100

    basic level, half part minEggDropper100 function solution

--- minEggDropper100.js
+++ minEggDropper100.js
@@ -1,30 +1,29 @@
 /** Basic Difficulty */
 
-/* I was rethinking the problem and I detected a problem, if I assume all start at 100 floor and 
-I only have one egg and I drop it, if it breaks I can't assume that the critical floor is where I am, 
-or is some of the floors below me. However, if I'm on the first floor and I drop an egg, if it breaks 
-I can assume that the critical floor is the zero floor. Is more logic for me and is a possibly start to 
-think in more eggs or infinite eggs. And the rules about what happens when an egg survives or not 
-(if it survives at n floor can survive at n-1, n-2 ... And if not can't survive at floors n+1, n+2) 
-are considered too.. */
+/* I realized that no matters how many eggs you have. Assuming the premise: "The eggs are all incredibly 
+similar – the effect of a fall from a particular floor is the same for all eggs." And assuming the premise: 
+"If an egg does not survive a fall from floor n, then it would also not survive a fall from the floors above 
+it: floor n+1, floor n+2, etc." too. The critical floor is the floor below where I droop the egg and it breaks. 
+Doesn't matter how many eggs I have because if one breaks I don't need the rest because the rest breaks too.*/
 
-function minEggDropper() {
-    let minDrops = 0;
+function minEggDropper100() {
+    let minDrops100 = 0;
 
     while (!eggBroken()) {
-        minDrops++;
+        minDrops100++;
     }
-    return minDrops;
+    return minDrops100;
 }
 
 /** Random function to simulate the consequence of egg drop */
 function eggBroken() {
     let value = Math.floor(Math.random() * 100 + 1);
+    console.log("IN!!!!!");
     return (value % 2 == 0);
 }
 
 function prueba() {
-    console.log(minEggDropper());
+    console.log(minEggDropper100());
 }
 
 prueba();
